43bb676c6183c20d654576edc320a7bc47ddb115
souhei hori <K024G0110@g.neec.ac.jp>
2025-02-13 14:41:19 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"");
+	win->CreateGameWindow(L"LC1A_21_ホリ_ソウヘイ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
